37dcae8bf8967a0af7221b71201f00b337609833
Kate <katerinkakorol25@gmail.com>
2021-11-30 13:59:27 +0200

    final

---
+++ js/Prices.js
@@ -0,0 +1,12 @@
+var linkClicked = document.getElementsByClassName('nav-link');
+var numClass = linkClicked.length;
+
+for (var i = 0; i < numClass; i++) {
+		linkClicked[i].addEventListener('click', function(){
+      var onTheMoment = document.getElementsByClassName('active');
+			onTheMoment[0].className = onTheMoment[0].className.replace(' active', '');
+			this.className += ' active';
+    }, false);
+	}
+
+
